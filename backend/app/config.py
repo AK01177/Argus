b1faff35ff784c37d0952ff0dfb829fbd0a24ebb
AK01177 <aryu7334@gmail.com>
2026-08-23 00:58:44 +0530

    setup database connection

--- backend/app/config.py
+++ backend/app/config.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     PROJECT_NAME: str="Argus"
     ENVIRONMENT: str="local"
+    DATABASE_URL: str="postgresql://postgres:postgres_password@localhost:5432/argus"
 
     model_config=SettingsConfigDict(
         env_file=".env",
